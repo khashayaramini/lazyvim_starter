1135fbc5db80fc1b336dd71d31ba2e0a62ad148f
khashayaramini <khashayaramini80@gmail.com>
2024-04-21 14:49:42 +0300

    harpoon added

--- lua/plugins/mine.lua
+++ lua/plugins/mine.lua
@@ -68,4 +68,48 @@ return {
       timeout = 0,
     },
   },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+    },
+    keys = function()
+      local keys = {
+        {
+          "<leader>H",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon File",
+        },
+        {
+          "<leader>h",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+        {
+          "<leader>m",
+          ":Telescope harpoon marks<CR>",
+          desc = "Telescope Harpoon",
+        },
+      }
+
+      for i = 1, 5 do
+        table.insert(keys, {
+          "<leader>" .. i,
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
+  },
 }
